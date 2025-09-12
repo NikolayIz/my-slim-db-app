41af4a80ba498df1b034b7386158ee1fad9dce8d
NikolayIz <nikolay97altov@gmail.com>
2025-09-12 11:41:21 +0300

    add init sql file

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$container = new Container();
+
+$container->set(\PDO::class, function () {
+    $conn = new \PDO('sqlite:' . __DIR__ . '/../var/database.sqlite');
+    $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    return $conn;
+});
+
+$initFilePath = __DIR__ . '/../init.sql';
+if (file_exists($initFilePath)) {
+    $initSql = file_get_contents($initFilePath);
+    $container->get(\PDO::class)->exec($initSql);
+}
+
+$app = AppFactory::createFromContainer($container);
